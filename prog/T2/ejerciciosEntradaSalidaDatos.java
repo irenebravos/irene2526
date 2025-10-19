74ea0c0bd5abd75b6d70e71a0aeb3fb7db08f302
irene <irenebravosanchez@iescastelar.com>
2025-10-19 22:59:14 +0500

    ejercicios

---
+++ prog/T2/ejerciciosEntradaSalidaDatos.java
@@ -0,0 +1,25 @@
+public class ejerciciosEntradaSalidaDatos {
+    public static void main(String[] args) {
+
+        //1. Declara un Scanner para leer desde teclado. Pide el nombre del usuario y guárdalo en una variable String. Luego muestra: "Hola, [nombre]".
+
+
+        //2. Pregúntale al usuario su edad y muestra por pantalla: "Tienes [edad] años."
+
+
+        //3. Pídele al usuario un número decimal y muéstralo luego por pantalla con 2 decimales (aunque tenga más o menos decimales).
+
+
+        //4. Pídele al usuario tres números enteros y muestra su suma.
+
+
+        //5. Pregúntale al usuario el radio de un círculo y muestra por pantalla su área y la longitud de su circunferencia (con tres decimales).
+
+
+        //6. Pregunta al usuario si es mayor de edad, guarda la respuesta en una variable Booleana y muéstrala por pantalla. (Ojo, el usuario responderá "Sí" o "No")
+
+
+        //7. Ticket: Pide al usuario el nombre de un producto, el número de unidades, el precio por unidad y luego muestra por pantalla el ticket de compra, con el nombre del producto, precio sin IVA, importe de IVA y total Todo bien formateado y bonito.
+
+    }
+}
